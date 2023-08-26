358e1a5e666157d6ebbe138412974ad31428fc43
Adeleye Ifaturoti <adeleyeifaturoti@yahoo.com>
2023-08-26 14:13:41 +0100

    react-router installed

---
+++ src/routes/root.tsx
@@ -0,0 +1,14 @@
+const Root = () => {
+  return (
+    <ul>
+      <li>
+        <a href="/">Home</a>
+      </li>
+      <li>
+        <a href="/hi">Hi</a>
+      </li>
+    </ul>
+  );
+};
+
+export default Root;
